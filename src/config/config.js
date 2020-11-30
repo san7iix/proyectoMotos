77b9686c70c73f14440bbb279f7a900c060a0742
Santiago Martinez <martinezsantiagowd@gmail.com>
2020-11-30 16:30:14 -0500

    Agregados primeros archivos

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+//=======================
+// puerto
+//=======================
+
+process.env.PORT = process.env.PORT || 3000
+
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+//Archivo de configuración
+app.use(require('./config/config'));
+
+//Configuración de la forma de mostrar los datos para el API
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+//Configuración del archivo de rutas
+// app.use(require('./routes/rutas'));
+
+//Inicio del servidor en el puerto para el app
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port ${process.env.PORT}`);
+});
